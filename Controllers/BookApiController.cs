6b6854eeba9304aa6a54b1f434354f5d74ed9cc7
Jeff Chen <zen00326598@gmail.com>
2024-10-03 16:26:18 +0800

    feat: 新增Book Controller

---
+++ Controllers/BookApiController.cs
@@ -0,0 +1,69 @@
+using IdentityJWTDemo.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityJWTDemo.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class BookApiController(
+    ApplicationDbContext dbContext
+) : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext = dbContext;
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+    {
+        return Ok(await _dbContext.Books.ToListAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Book>> GetBook(int id)
+    {
+        var book = await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        return Ok(book);
+    }
+    [HttpPost]
+    public async Task<ActionResult<Book>> CreateBook(BookParameter parameter)
+    {
+        var book = new Book
+        {
+            Title = parameter.Title
+        };
+        _dbContext.Books.Add(book);
+        await _dbContext.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
+    {
+        var book = await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+        if (book == null)
+            return NotFound();
+
+        book.Title = updatedBook.Title;
+        await _dbContext.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBookAsync(int id)
+    {
+        var book = await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+
+        if (book == null)
+            return NotFound();
+
+        _dbContext.Books.Remove(book);
+        await _dbContext.SaveChangesAsync();
+        return Ok();
+    }
+}
+
---
+++ Controllers/Parameters/BookParameter.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityJWTDemo;
+
+public class BookParameter
+{
+  [Required]
+  public string Title { get; set; } = null!;
+}
