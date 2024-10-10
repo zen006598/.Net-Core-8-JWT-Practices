854d5cdd8d98b033d6acd68b1b05d4cf8eeebd2c
Jeff Chen <zen00326598@gmail.com>
2024-10-10 18:25:53 +0800

    feat: 新增JWT Setting Class

---
+++ Configurations/JWTSetting.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityJWTDemo.Configurations;
+
+public class JWTSetting
+{
+  public string Secret { get; set; } = null!;
+  public string ValidIssuer { get; set; } = null!;
+  public string ValidAudience { get; set; } = null!;
+  public int TokenExpirationInMinutes { get; set; }
+  public int RefreshTokenExpirationInMonths { get; set; }
+}
