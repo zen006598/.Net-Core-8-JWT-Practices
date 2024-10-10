31e9fa3262eab151c0c5698c0e552315b34f1af7
Jeff Chen <zen00326598@gmail.com>
2024-10-10 18:26:46 +0800

    refactor: 將Controller 中邏輯抽離至Service

--- Controllers/AuthenticateApiController.cs
+++ Controllers/AuthenticateApiController.cs
@@ -1,14 +1,7 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using IdentityJWTDemo.Data;
-using IdentityJWTDemo.Models;
 using IdentityJWTDemo.Parameters;
-using IdentityJWTDemo.ViewModels;
+using IdentityJWTDemo.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 namespace IdentityJWTDemo.Controllers;
 
@@ -17,29 +10,22 @@ namespace IdentityJWTDemo.Controllers;
 public class AuthenticateApiController(
     UserManager<IdentityUser> userManager,
     RoleManager<IdentityRole> roleManager,
-    IConfiguration configuration,
     ILogger<AuthenticateApiController> logger,
-    ApplicationDbContext dbContext,
-    TokenValidationParameters tokenValidationParams
+    ITokenService tokenService
 ) : ControllerBase
 {
     private readonly UserManager<IdentityUser> _userManager = userManager;
     private readonly RoleManager<IdentityRole> _roleManager = roleManager;
-    private readonly IConfiguration _configuration = configuration;
     private ILogger<AuthenticateApiController> _logger = logger;
-    private ApplicationDbContext _dbContext = dbContext;
-    private readonly TokenValidationParameters _tokenValidationParams = tokenValidationParams;
-
-    private readonly int AuthTokenExpireTimeMins = 15;
-    private readonly int RefreshTokenExpireTimeMonths = 6;
+    private ITokenService _tokenService = tokenService;
 
     [HttpPost("Login")]
-    public async Task<ActionResult> Login([FromBody] LoginParameter loginParameters)
+    public async Task<IActionResult> Login([FromBody] LoginParameter loginParameters)
     {
         var user = await _userManager.FindByEmailAsync(loginParameters.Email);
         if (user != null && await _userManager.CheckPasswordAsync(user, loginParameters.Password))
         {
-            return Ok(await CreateTokenAsync(user));
+            return Ok(await _tokenService.CreateTokenAsync(user));
         }
         return Unauthorized();
     }
@@ -88,211 +74,21 @@ public class AuthenticateApiController(
         return Ok(new { Status = "Success", Message = "User created successfully!" });
     }
 
-    [HttpGet("RefreshToken")]
-    public async Task<ActionResult> RefreshToken(TokenParameter parameter)
+    [HttpPost("RefreshToken")]
+    public async Task<ActionResult> RefreshToken([FromBody] TokenParameter parameter)
     {
-        if (ModelState.IsValid)
+        if (!ModelState.IsValid)
         {
-            var result = await VerifyAndGenerateToken(parameter);
-
-            if (result == null)
-                return BadRequest("Invalid tokens");
-
-            return Ok(result);
+            return BadRequest("Invalid payload");
         }
 
-        return BadRequest("Invalid payload");
-    }
+        var result = await _tokenService.VerifyAndGenerateToken(parameter);
 
-    private async Task<AuthViewModel> CreateTokenAsync(IdentityUser user)
-    {
-        var userRoles = await _userManager.GetRolesAsync(user);
-
-        var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name, user.UserName),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                };
-
-        foreach (var userRole in userRoles)
+        if (result == null)
         {
-            claims.Add(new Claim(ClaimTypes.Role, userRole));
+            return BadRequest("Invalid tokens");
         }
 
-        var token = CreateJwtSecurityToken(claims);
-        var refreshToken = await CreateRefreshToken(token, user);
-
-        return new AuthViewModel
-        {
-            Token = new JwtSecurityTokenHandler().WriteToken(token),
-            RefreshToken = refreshToken.Token,
-            Success = true
-        };
-    }
-
-    private JwtSecurityToken CreateJwtSecurityToken(List<Claim> claims)
-    {
-        var key = _configuration.GetValue<string>("JwtSettings:Secret") ?? throw new ArgumentNullException("JwtSettings:Secret");
-        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
-
-        var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha512Signature);
-
-        var token = new JwtSecurityToken(
-            issuer: _configuration.GetValue<string>("JwtSettings:ValidIssuer"),
-            audience: _configuration.GetValue<string>("JwtSettings:ValidAudience"),
-            expires: DateTime.Now.AddMinutes(AuthTokenExpireTimeMins),
-            claims: claims,
-            signingCredentials: credentials);
-        return token;
-    }
-
-    private async Task<RefreshToken> CreateRefreshToken(JwtSecurityToken token, IdentityUser user)
-    {
-        var refreshToken = new RefreshToken()
-        {
-            JwtId = token.Id,
-            IsUsed = false,
-            IsRevorked = false,
-            UserId = user.Id,
-            AddedDate = DateTime.UtcNow,
-            ExpiryDate = DateTime.UtcNow.AddMonths(RefreshTokenExpireTimeMonths),
-            Token = RandomString(25) + Guid.NewGuid()
-        };
-
-        await _dbContext.RefreshTokens.AddAsync(refreshToken);
-        await _dbContext.SaveChangesAsync();
-
-        return refreshToken;
-    }
-
-    private string RandomString(int length)
-    {
-        var random = new Random();
-        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        return new string(Enumerable.Repeat(chars, length)
-            .Select(x => x[random.Next(x.Length)]).ToArray());
-    }
-
-    private async Task<AuthViewModel> VerifyAndGenerateToken(TokenParameter parameter)
-    {
-        var jwtTokenHandler = new JwtSecurityTokenHandler();
-
-        try
-        {
-            // Validation 1 - Validation JWT token format
-            var tokenInVerification = jwtTokenHandler.ValidateToken(parameter.Token, _tokenValidationParams, out var validatedToken);
-
-            // Validation 2 - Validate encryption alg
-            if (validatedToken is JwtSecurityToken jwtSecurityToken)
-            {
-                var result = jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase);
-
-                if (!result)
-                    return null;
-            }
-
-            // Validation 3 - validate expiry date
-            var utcExpiryDate = long.Parse(tokenInVerification.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Exp).Value);
-
-            var expiryDate = UnixTimeStampToDateTime(utcExpiryDate);
-
-            if (expiryDate > DateTime.UtcNow)
-            {
-                return new AuthViewModel()
-                {
-                    Success = false,
-                    Errors = new List<string>() { "Token has not yet expired" }
-                };
-            }
-
-            // validation 4 - validate existence of the token
-            var storedRefreshToken = await _dbContext.RefreshTokens.FirstOrDefaultAsync(x => x.Token == parameter.RefreshToken);
-
-            if (storedRefreshToken == null)
-            {
-                return new AuthViewModel()
-                {
-                    Success = false,
-                    Errors = new List<string>() { "Refresh Token does not exist" }
-                };
-            }
-
-            // Validation 5 - 检查存储的 RefreshToken 是否已过期
-            // Check the date of the saved refresh token if it has expired
-            if (DateTime.UtcNow > storedRefreshToken.ExpiryDate)
-            {
-                return new AuthViewModel()
-                {
-                    Success = false,
-                    Errors = new List<string>() { "Refresh Token has expired, user needs to re-login" }
-                };
-            }
-
-            // Validation 6 - validate if used
-            if (storedRefreshToken.IsUsed)
-            {
-                return new AuthViewModel()
-                {
-                    Success = false,
-                    Errors = new List<string>() { "Refresh Token has been used" }
-                };
-            }
-
-            // Validation 7 - validate if refresh token has revoked
-            if (storedRefreshToken.IsRevorked)
-            {
-                return new AuthViewModel()
-                {
-                    Success = false,
-                    Errors = new List<string>() { "Refresh Token has been revoked" }
-                };
-            }
-
-            // Validation 8 - validate JWT token Id
-            var jti = tokenInVerification.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Jti).Value;
-
-            if (storedRefreshToken.JwtId != jti)
-            {
-                return new AuthViewModel()
-                {
-                    Success = false,
-                    Errors = new List<string>() { "The token doesn't matched the saved token" }
-                };
-            }
-
-            // update current token to be used
-            storedRefreshToken.IsUsed = true;
-            _dbContext.RefreshTokens.Update(storedRefreshToken);
-            await _dbContext.SaveChangesAsync();
-
-            // 生成一个新的 token
-            var user = await _userManager.FindByIdAsync(storedRefreshToken.UserId);
-            return await CreateTokenAsync(user);
-        }
-        catch (Exception ex)
-        {
-            if (ex.Message.Contains("Lifetime validation failed. The token is expired."))
-            {
-                return new AuthViewModel()
-                {
-                    Success = false,
-                    Errors = new List<string>() { "Token has expired please re-login" }
-                };
-            }
-            else
-            {
-                return new AuthViewModel()
-                {
-                    Success = false,
-                    Errors = new List<string>() { "Something went wrong." }
-                };
-            }
-        }
-    }
-    private DateTime UnixTimeStampToDateTime(long unixTimeStamp)
-    {
-        var dateTimeVal = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTimeVal = dateTimeVal.AddSeconds(unixTimeStamp).ToLocalTime();
-        return dateTimeVal;
+        return Ok(result);
     }
 }
---
+++ Services/TokenService.cs
@@ -0,0 +1,248 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using IdentityJWTDemo.Configurations;
+using IdentityJWTDemo.Data;
+using IdentityJWTDemo.Models;
+using IdentityJWTDemo.Parameters;
+using IdentityJWTDemo.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdentityJWTDemo.Services;
+public interface ITokenService
+{
+  Task<AuthViewModel> CreateTokenAsync(IdentityUser user);
+  Task<AuthViewModel> VerifyAndGenerateToken(TokenParameter parameter);
+  Task<RefreshToken> CreateRefreshToken(string jwtId, string userId);
+  Task<bool> ValidateRefreshToken(string refreshToken, string jwtId);
+}
+public class TokenService(
+    UserManager<IdentityUser> userManager,
+    JWTSetting jwtSettings,
+    TokenValidationParameters tokenValidationParameters,
+    ApplicationDbContext dbContext,
+    ILogger<TokenService> logger
+) : ITokenService
+{
+  private readonly UserManager<IdentityUser> _userManager = userManager;
+  private readonly JWTSetting _jwtSettings = jwtSettings;
+  private readonly TokenValidationParameters _tokenValidationParameters = tokenValidationParameters;
+  private readonly ApplicationDbContext _dbContext = dbContext;
+  private readonly ILogger<TokenService> _logger = logger;
+
+  public async Task<AuthViewModel> CreateTokenAsync(IdentityUser user)
+  {
+    var jwtToken = await CreateJwtToken(user);
+    var refreshToken = await CreateRefreshToken(jwtToken.Id, user.Id);
+
+    return new AuthViewModel
+    {
+      Token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+      RefreshToken = refreshToken.Token,
+      Success = true
+    };
+  }
+
+  public async Task<AuthViewModel> VerifyAndGenerateToken(TokenParameter parameter)
+  {
+    var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+    try
+    {
+      // Validate JWT token format(JWS/JWE)
+      var tokenInVerification = jwtTokenHandler.ValidateToken(parameter.Token, _tokenValidationParameters, out var validatedToken);
+
+      // Validate encryption algorithm
+      if (validatedToken is JwtSecurityToken jwtSecurityToken)
+      {
+        var result = jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase);
+        if (!result) return null;
+      }
+
+      // Validate expiry date
+      // get the expire time from claim, which is in Unix time format
+      var utcExpiryDate = long.Parse(tokenInVerification.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Exp).Value);
+      var expiryDate = UnixTimeStampToDateTime(utcExpiryDate);
+      if (expiryDate > DateTime.UtcNow)
+      {
+        return new AuthViewModel
+        {
+          Success = false,
+          Errors = new List<string> { "Token has not yet expired" }
+        };
+      }
+
+      // Validate refresh token
+      var storedRefreshToken = await _dbContext.RefreshTokens.FirstOrDefaultAsync(x => x.Token == parameter.RefreshToken);
+      if (storedRefreshToken == null)
+      {
+        return new AuthViewModel
+        {
+          Success = false,
+          Errors = new List<string> { "Refresh Token does not exist" }
+        };
+      }
+
+      // Check if refresh token has expired
+      if (DateTime.UtcNow > storedRefreshToken.ExpiryDate)
+      {
+        return new AuthViewModel
+        {
+          Success = false,
+          Errors = new List<string> { "Refresh Token has expired, user needs to re-login" }
+        };
+      }
+
+      // Check if refresh token has been used
+      if (storedRefreshToken.IsUsed)
+      {
+        return new AuthViewModel
+        {
+          Success = false,
+          Errors = new List<string> { "Refresh Token has been used" }
+        };
+      }
+
+      // Check if refresh token has been revoked
+      if (storedRefreshToken.IsRevoked)
+      {
+        return new AuthViewModel
+        {
+          Success = false,
+          Errors = new List<string> { "Refresh Token has been revoked" }
+        };
+      }
+
+      // Validate JWT token Id
+      var jti = tokenInVerification.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Jti).Value;
+      if (storedRefreshToken.JwtId != jti)
+      {
+        return new AuthViewModel
+        {
+          Success = false,
+          Errors = new List<string> { "The token doesn't match the saved token" }
+        };
+      }
+
+      // Update current token to be used
+      storedRefreshToken.IsUsed = true;
+      _dbContext.RefreshTokens.Update(storedRefreshToken);
+      await _dbContext.SaveChangesAsync();
+
+      // Generate new tokens
+      var user = await _userManager.FindByIdAsync(storedRefreshToken.UserId);
+      return await CreateTokenAsync(user);
+    }
+    catch (Exception ex)
+    {
+      _logger.LogError($"Error occurred while verifying the token {ex}");
+      return new AuthViewModel
+      {
+        Success = false,
+        Errors = new List<string> { "Token validation failed" }
+      };
+    }
+  }
+
+  public async Task<RefreshToken> CreateRefreshToken(string jwtId, string userId)
+  {
+    var refreshToken = new RefreshToken
+    {
+      JwtId = jwtId,
+      IsUsed = false,
+      IsRevoked = false,
+      UserId = userId,
+      AddedDate = DateTime.UtcNow,
+      ExpiryDate = DateTime.UtcNow.AddMonths(_jwtSettings.RefreshTokenExpirationInMonths),
+      Token = GenerateRefreshTokenString()
+    };
+
+    await _dbContext.RefreshTokens.AddAsync(refreshToken);
+    await _dbContext.SaveChangesAsync();
+
+    return refreshToken;
+  }
+
+  public async Task<bool> ValidateRefreshToken(string refreshToken, string jwtId)
+  {
+    var storedRefreshToken = await _dbContext.RefreshTokens.FirstOrDefaultAsync(x => x.Token == refreshToken);
+
+    if (storedRefreshToken == null)
+    {
+      return false;
+    }
+
+    if (DateTime.UtcNow > storedRefreshToken.ExpiryDate)
+    {
+      return false;
+    }
+
+    if (storedRefreshToken.IsUsed)
+    {
+      return false;
+    }
+
+    if (storedRefreshToken.IsRevoked)
+    {
+      return false;
+    }
+
+    if (storedRefreshToken.JwtId != jwtId)
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  private async Task<JwtSecurityToken> CreateJwtToken(IdentityUser user)
+  {
+    var userClaims = await _userManager.GetClaimsAsync(user);
+    var roles = await _userManager.GetRolesAsync(user);
+
+    var claims = new List<Claim>
+    {
+      new Claim(ClaimTypes.Name, user.UserName),
+      new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+      new Claim(ClaimTypes.NameIdentifier, user.Id)
+    };
+
+    claims.AddRange(userClaims);
+    foreach (var role in roles)
+    {
+      claims.Add(new Claim(ClaimTypes.Role, role));
+    }
+
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret));
+    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+    var token = new JwtSecurityToken(
+      issuer: _jwtSettings.ValidIssuer,
+      audience: _jwtSettings.ValidAudience,
+      claims: claims,
+      expires: DateTime.Now.AddMinutes(_jwtSettings.TokenExpirationInMinutes),
+      signingCredentials: creds);
+
+    return token;
+  }
+
+  private string GenerateRefreshTokenString()
+  {
+    var randomNumber = new byte[32];
+    using (var rng = RandomNumberGenerator.Create())
+    {
+      rng.GetBytes(randomNumber);
+      return Convert.ToBase64String(randomNumber);
+    }
+  }
+
+  private DateTime UnixTimeStampToDateTime(long unixTimeStamp)
+  {
+    var dateTimeVal = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+    dateTimeVal = dateTimeVal.AddSeconds(unixTimeStamp).ToUniversalTime();
+    return dateTimeVal;
+  }
+}
